30fbdc96a880ed51416fbdfc00d76cf7260d338e
sjvasconcello <sjvasconcello@gmail.com>
2019-08-11 02:43:02 -0400

    fazt tutorial ready :D

--- fazt_tutorial/app/lib/main.dart
+++ fazt_tutorial/app/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
 void main() {
   runApp(
@@ -14,10 +17,60 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Map data;
+  List usersData;
+
+  getUsers() async {
+    http.Response response = await http.get('http://10.0.2.2:4000/api/users');
+    data = json.decode(response.body);
+    setState(() {
+      usersData = data['users'];
+    });
+  }
+
+  @override
+  initState() {
+    super.initState();
+    getUsers();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Lista de usuarios'),
+        backgroundColor: Colors.indigo[900],
+      ),
+      body: ListView.builder(
+        itemCount: usersData == null ? 0 : usersData.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Text(
+                    "$index",
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                CircleAvatar(
+                  backgroundImage: NetworkImage(usersData[index]['avatar']),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    "${usersData[index]["firstName"]}"
+                    "${usersData[index]["lastName"]}",
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.w700),
+                  ),
+                ),
+              ]),
+            ),
+          );
+        },
       ),
     );
   }
