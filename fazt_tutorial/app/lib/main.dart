7c57fc6890cd534932c267eb7ebe73142eda42e5
sjvasconcello <sjvasconcello@gmail.com>
2019-08-11 01:42:03 -0400

    i dont have any idea

--- fazt_tutorial/app/lib/main.dart
+++ fazt_tutorial/app/lib/main.dart
@@ -17,7 +17,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('User List'),
+        title: Text('Lista de usuarios'),
       ),
     );
   }
